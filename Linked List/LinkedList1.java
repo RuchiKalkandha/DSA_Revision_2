bdaa756363adccbfea5dd630c02d3fbe1cbfb8d9
RuchiKalkandha <ruchikalkandha@gmail.com>
2025-02-21 21:10:27 +0530

    linked list basic questions revision

--- Linked List/LinkedList1.java
+++ Linked List/LinkedList1.java
@@ -22,9 +22,11 @@ public class LinkedList1 {
 
     public static Node head;
     public static Node tail;
+    public static int size;
 
     // method to add node at first  // time complexity O(1)
     public void addFirst(int data){
+        size++;
         // creating new node
         Node newNode = new Node(data);
         // if node empty:
@@ -42,6 +44,7 @@ public class LinkedList1 {
     // method to add node at last  //O(n) time complexity
     // instead of using head, if we had a tail pointer given it would have a time complexity O(1)
     public void addLast(int data){
+        size++;
         Node newNode = new Node(data);
         if(head == null){
             head = tail = newNode;
@@ -57,16 +60,81 @@ public class LinkedList1 {
 
     // printing a linked list 
 
-    public void printinfLinkedList(Node head){
+    public void printinfLinkedList(){
         if(head == null){
             System.out.println("Linked List is empty");
             return;
         }
         Node headCopy = head;
         while(headCopy != null){
-            System.out.println(headCopy.data);
+            System.out.print(headCopy.data + " ");
             headCopy = headCopy.next;
         }
+        System.out.println();
+        System.out.println("size of Linked List is: " + size);
+    }
+
+    // adding an element in middle of Linked List
+    public void insertAtMiddle(int index, int data){
+        Node newNode = new Node(data);
+        Node headCopy = head;
+        if(index == 0){
+            addFirst(data);
+            return;
+        }
+        size++;
+        int i = 0;
+        while(index-1 != i){
+            headCopy = headCopy.next;
+            i++;
+        }
+       
+        newNode.next = headCopy.next;
+        headCopy.next = newNode;
+    }
+
+    public int SizeofLL(){
+        int i =0;
+        Node headCopy = head;
+        while(headCopy != null){
+            headCopy = headCopy.next;
+            i++;
+        }
+        return i;
+    }
+
+    // removing node from beginning of linked list //
+    public void removeFirst(){
+        if(size != 0){
+            size--;
+        }
+        if(head.next == null || head == null){
+            head = tail = null;
+            return;
+        }
+
+        head = head.next;
+        // garbage collector will delete the prev node automatically.
+        // as there is no way we can reach to this node as there is no link back
+    }
+
+    // remove from last
+    public void removeLast(){
+        if(head == null){
+            return;
+        }
+        if(head.next == null){
+            size--;
+            head = tail = null;
+            return;
+        }
+        Node headCopy=head;
+        size--;
+        while(headCopy.next.next != null){
+            headCopy = headCopy.next;
+        }
+        headCopy.next = null;
+        tail = headCopy.next;
     }
 
 
@@ -76,6 +144,19 @@ public class LinkedList1 {
         ll1.addFirst(1);
         ll1.addLast(3);
         ll1.addLast(4);
-        ll1.printinfLinkedList(head);
+        ll1.printinfLinkedList();
+        System.out.println(ll1.SizeofLL());
+        ll1.insertAtMiddle(3, 9);
+        ll1.printinfLinkedList();
+        System.out.println(ll1.SizeofLL());
+        ll1.insertAtMiddle(0, 0);
+        ll1.printinfLinkedList();
+        System.out.println(ll1.SizeofLL());
+        ll1.removeFirst();
+        ll1.printinfLinkedList();
+        System.out.println(ll1.SizeofLL());
+        ll1.removeLast();
+        ll1.printinfLinkedList();
+        System.out.println(ll1.SizeofLL());
     }
 }
