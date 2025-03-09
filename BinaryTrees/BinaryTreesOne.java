f84a0b2b02685af6e6a6dd185b3a56170a4b28ad
RuchiKalkandha <ruchikalkandha@gmail.com>
2025-03-10 00:37:30 +0530

    top view of a binary tree

--- BinaryTrees/BinaryTreesOne.java
+++ BinaryTrees/BinaryTreesOne.java
@@ -158,6 +158,48 @@ class Solution {
         } 
         return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
     }
+    // Top view of Binary Tree
+    class info2{
+        Node node;
+        int hd;
+        public info2(Node node, int hd){
+            this.node = node;
+            this.hd = hd;
+        }
+    }
+    public void topView(Node root){
+        // Level Order
+        Queue<info2> q = new LinkedList<>();
+        HashMap<Integer, Node> map = new HashMap<>();
+        int min = 0, max = 0;
+        q.add(new info2(root, 0));
+        q.add(null);
+
+        while(!q.isEmpty()){
+            info2 curr = q.remove();
+            if(curr == null){
+                if(q.isEmpty()) break;
+                else q.add(null);
+            }else{
+            if(!map.containsKey(curr.hd)){ // first occurance of hd
+                map.put(curr.hd, curr.node);
+            }
+            if(curr.node.left != null){
+                q.add(new info2(curr.node.left, curr.hd-1));
+                min = Math.min(min, curr.hd-1);
+            }
+            if(curr.node.right != null){
+                q.add(new info2(curr.node.right, curr.hd+1));
+                max = Math.max(max, curr.hd+1);
+            }
+        }
+
+        }
+        for(int i = min; i<=max; i++){
+            System.out.print(map.get(i).data + " ");
+        }
+        System.out.println();
+    }
 }
     public static void main(String args[]){
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -177,5 +219,7 @@ class Solution {
         System.out.print(tree.nodesCount(root));
         System.out.println();
         System.out.print(tree.nodesSum(root));
+        System.out.println();
+        tree.topView(root);
     }
 }
